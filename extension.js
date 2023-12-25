5822832f9e95a1c5c1da6a799f368899a612188c
Igidali <d3facerx@gmail.com>
2023-12-25 21:12:36 +0400

    feat: creating file and folder

--- extension.js
+++ extension.js
@@ -30,16 +30,15 @@ function activate(context) {
 			// console.log(match)
             if (match) {
                 const componentName = match[1];
+				const currentFile = editor.document.uri.fsPath
 				const filePath = match[2].length > 1 ? match[2] : String(match)
 				const filePathSplitted = filePath.split('/')
-                const fileName = filePathSplitted[filePathSplitted.length - 1]; 
-				console.log(filePath, fileName)
+                const fileName = match[2].length > 1 ? filePathSplitted[filePathSplitted.length - 1] + path.extname(currentFile) : filePathSplitted[filePathSplitted.length - 1]; 
 
 				if(filePath && isRelativePath(filePath)) { 
 				// Create the folder and file
-					const currentFileFolder = editor.document.uri.fsPath
 					// const rootPath = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
-					const rootPath = path.dirname(currentFileFolder)
+					const rootPath = path.dirname(currentFile)
             	    createFolderAndFile(rootPath, filePath, fileName);
 				} else {
 					vscode.window.showInformationMessage('Cannot create components for external packages or libraries.');
@@ -62,25 +61,24 @@ function isRelativePath(filePath) {
 function createFolderAndFile(rootPath, filePath, fileName) {
     const folderPath = path.dirname(filePath);
     const fullFolderPath = path.join(rootPath, folderPath);
-    const fullFilePath = path.join(fullFolderPath, fileName);  
-	// console.log(fullFilePath)
-	// console.log(fullFolderPath)
-    // Check if the folder exists, create it if not
-    // if (!fs.existsSync(fullFolderPath)) {
-    //     fs.mkdirSync(fullFolderPath, { recursive: true });
-    //     vscode.window.showInformationMessage(`Folder created: ${folderPath}`);
-    // }
+    const fullFilePath = path.join(fullFolderPath, fileName);   
 
-    // // Check if the file exists, create it if not
-    // if (!fs.existsSync(fullFilePath)) {
-    //     fs.writeFileSync(
-    //         fullFilePath,
-    //         `import React from 'react';\n\nfunction ${fileName}() {\n  return (\n    <div>\n      {/* ${componentName} component */}\n    </div>\n  );\n}\n\nexport default ${componentName};\n`
-    //     );
-    //     vscode.window.showInformationMessage(`File created: ${filePath}`);
-    // } else {
-    //     vscode.window.showInformationMessage(`File already exists: ${filePath}`);
-    // }
+    // Check if the folder exists, create it if not
+    if (!fs.existsSync(fullFolderPath)) {
+        fs.mkdirSync(fullFolderPath, { recursive: true });
+        vscode.window.showInformationMessage(`Folder created: ${folderPath}`);
+    }
+
+    // Check if the file exists, create it if not
+    if (!fs.existsSync(fullFilePath)) {
+        fs.writeFileSync(
+            fullFilePath,
+            `import React from 'react';\n\nfunction ${fileName.split('.')[0]}() {\n  return (\n    <div>\n      {/* ${fileName.split('.')[0]} component */}\n    </div>\n  );\n}\n\nexport default ${fileName.split('.')[0]};\n`
+        );
+        vscode.window.showInformationMessage(`File created: ${filePath}`);
+    } else {
+        vscode.window.showInformationMessage(`File already exists: ${filePath}`);
+    }
 }
 // This method is called when your extension is deactivated
 function deactivate() {}
