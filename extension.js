d6c009074469b7e3f1cc4ee67a52da026f0d51f4
Igidali <d3facerx@gmail.com>
2023-12-27 23:10:11 +0400

    changed text template for created file. And command execution on startup

--- extension.js
+++ extension.js
@@ -9,52 +9,32 @@ const path = require('path');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+function activate(context) { 
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "react-import-helper" is now active!');
-
-    let disposable = vscode.workspace.onDidChangeTextDocument((event) => {
-        const editor = vscode.window.activeTextEditor;
-		vscode.window.showInformationMessage('Hello World from React Import Helper!');
-        if (editor && event.document === editor.document) {
-            // Check if new lines have been added at the end of the document
-            const lastLine = editor.document.lineAt(editor.document.lineCount - 1).range;
-            const changes = event.contentChanges.filter(change => lastLine.contains(change.range.end));
-			// console.log(lastLine, changes)
-
-            if (changes.length > 0) {
-                // Automatically trigger the extension's command
-                vscode.commands.executeCommand('extension.importHelper');
-            }
-        }
-    });
-
-	context.subscriptions.push(disposable);
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let commandDisposable = vscode.commands.registerCommand('extension.importHelper', function () {
-		// The code you place here will be executed every time your command is executed
+ 	let disposable = vscode.commands.registerCommand('extension.importHelper', function () {
         const editor = vscode.window.activeTextEditor; 
+
         if (editor) {
+
             // Get the selected text 
 			const lineNumber = editor.selection.active.line;
 			// Get the entire line's text
 			const lineText = editor.document.lineAt(lineNumber).text;
 			const match = !lineText.includes('from') ? lineText.replace(/(import|')/g, '').trim() : lineText.match(/import (\{.*\}|\w+) from ['"](.+)['"]/);  
- 
+            // const isEnterPressed = event.selections.some(sel => sel.isSingleLine && sel.active.character === 0);
+            // console.log(isEnterPressed)
+            
             if (match && match.length >= 2) { 
 				const currentFile = editor.document.uri.fsPath
 				const filePath = match[2].length > 1 ? match[2] : String(match)
-				const filePathSplitted = filePath.split('/')
-                const fileName = match[2].length > 1 ? filePathSplitted[filePathSplitted.length - 1] + path.extname(currentFile) : filePathSplitted[filePathSplitted.length - 1]; 
-
+				const filePathSplitted = !filePath.endsWith('/') ? filePath.split('/') : "undefined"
+                const fileName = filePathSplitted === "undefined" ? filePathSplitted : match[2].length > 1 ? filePathSplitted[filePathSplitted.length - 1] + path.extname(currentFile) : filePathSplitted[filePathSplitted.length - 1]; 
+                const componentNameRaw = match[1]
+                const componentName = match[1].includes('}') ? match[1].replace(/({|})/g, '').split(',') : [match[1].replace("'", '')   ]
 				if(filePath && isRelativePath(filePath)) { 
 				// Create the folder and file 
 					const rootPath = path.dirname(currentFile)
-            	    createFolderAndFile(rootPath, filePath, fileName);
+            	    createFolderAndFile(rootPath, filePath, fileName, componentName,componentNameRaw);
 				} else {
 					vscode.window.showInformationMessage('Cannot create components for external packages or libraries.');
 				}
@@ -63,20 +43,41 @@ function activate(context) {
                 vscode.window.showInformationMessage('No import statement found on the current line.');
             } 
 		}
-		// Display a message box to the user
+		// Display a message box
 		vscode.window.showInformationMessage('Hello World from React Import Helper!');
 	});
+ 
 
-	context.subscriptions.push(commandDisposable);
+	context.subscriptions.push(disposable);
+
+    vscode.workspace.onDidChangeTextDocument((event) => {
+        const editor = vscode.window.activeTextEditor;  
+        if (editor && event.document === editor.document) {
+            // Check if the last line has changed 
+            const lineNumber = editor.selection.active.line;
+            let incNumber = lineNumber + 1
+            const currentLine =  editor.document.lineAt(lineNumber).text;
+            const nextLine = editor.document.lineAt(lineNumber + 1).text; 
+            console.log(currentLine, nextLine) 
+            if(editor.document.lineAt(lineNumber).lineNumber + 1 === editor.document.lineAt(lineNumber).lineNumber) {
+                console.log(incNumber)
+            }
+            // Trigger the command if the last line is empty (user pressed Enter at the end)
+            if (currentLine.match(/import (\{.*\}|\w+) from ['"](.+)['"]/)) {
+                vscode.commands.executeCommand('extension.importHelper');
+            }
+        }
+    });
+    // vscode.commands.executeCommand("extension.importHelper");
 }
 function isRelativePath(filePath) {
     // Check if the file path is relative (starts with a dot or a slash)
     return filePath.startsWith('.') || filePath.startsWith('/');
 }
-function createFolderAndFile(rootPath, filePath, fileName) {
-    const folderPath = path.dirname(filePath);
+function createFolderAndFile(rootPath, filePath, fileName, componentName, singleComponent) {
+    const folderPath = filePath.endsWith('/') ? filePath : path.dirname(filePath);
     const fullFolderPath = path.join(rootPath, folderPath);
-    const fullFilePath = path.join(fullFolderPath, fileName);   
+    const fullFilePath = path.join(fullFolderPath, fileName);    
 
     // Check if the folder exists, create it if not
     if (!fs.existsSync(fullFolderPath)) {
@@ -85,15 +86,30 @@ function createFolderAndFile(rootPath, filePath, fileName) {
     }
 
     // Check if the file exists, create it if not
-    if (!fs.existsSync(fullFilePath)) {
-        fs.writeFileSync(
-            fullFilePath,
-            `import React from 'react';\n\nfunction ${fileName.split('.')[0]}() {\n  return (\n    <div>\n      {/* ${fileName.split('.')[0]} component */}\n    </div>\n  );\n}\n\nexport default ${fileName.split('.')[0]};\n`
-        );
-        vscode.window.showInformationMessage(`File created: ${filePath}`);
+
+    if (fileName !== "undefined" ) {
+        if (!fs.existsSync(fullFilePath) ) {
+            let importText = `import React from 'react';\n`;
+            let exportText = `\n\nexport default ${componentName.join('')};\n`;
+            if(componentName.length > 1) {
+                exportText = `\n\nexport ${singleComponent};\n`;
+            }
+            fs.appendFileSync(fullFilePath, importText)
+            componentName.map(compname => {
+                fs.appendFileSync(
+                    fullFilePath,
+                    `\nfunction ${compname}() {\n  return (\n    <div>\n      {/* ${compname} component */}\n    </div>\n  );\n}`
+                );
+            })
+            fs.appendFileSync(fullFilePath, exportText)
+            vscode.window.showInformationMessage(`File created: ${filePath}`);
+        } else {
+            vscode.window.showInformationMessage(`File already exists: ${filePath}`);
+        }
     } else {
-        vscode.window.showInformationMessage(`File already exists: ${filePath}`);
+        // vscode.window.showInformationMessage(`Please write filename!`);
     }
+
 }
 // This method is called when your extension is deactivated
 function deactivate() {}
